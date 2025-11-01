045bc9ae881fd61638b7b62f34a44d51d0e7133f
M Hussain Shahzad <hussainshahzad7744@gmail.com>
2025-11-01 09:53:18 +0500

    session method in express.js

--- index.js
+++ index.js
@@ -1,48 +1,59 @@
-// index.js
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require("cors");
+const cors = require('cors');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
-const{body,validationResult}=require('express-validator');
-// Middleware
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-const path = require('path'); 
-const multer= require('multer')
-app.use(express.json());
+
 app.use(cors());
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.get('/go', (req, res) => {
+//   res.render('login');
+// });
+const session=require('express-session');
+const { error } = require('jquery');
+app.use(session({
+  secret:'secreat',
+  resave:'false',
+  saveUninitialized:'false',
+  cooki:{maxAge:1000*60*60*24}
+}))
 
-const storage=multer.diskStorage({
-  destination:(req,file,cb)=>{
-    cb(null,'uploads/')
-  },
-  filename:(req,file,cb)=>{
-    cb(null,Date.now()+'-'+file.originalname)
-  }
-
-})
-const fileFilter=(req,file,cb)=>{
-  if(file.mimetype==='image/png'){ 
-    cb(null,true)
-  }else{
-    cb(new Error('Only jpeg files are allowed'),false)
-  }
+app.get('/', (req, res) => {
+  if (req.session.username) {
+  res.send("hy i am hussain")
+}else{
+  res.send('no user name')
 }
-const upload=multer({
-  storage:storage,
-  limits:{fileSize:1024*1024*5},
-fileFilter:fileFilter
+}
+);
 
-})
 
-app.get('/',(req,res)=>{
-  res.render('login')
-})
-app.post('/myForm',upload.single('profile'),(req,res)=>{
-res.send(req.file)
+app.get('/username',(req,res)=>{
+  req.session.username='HUssain'
+  res.send('username is sent')
 })
 
 
-// Start Server
-app.listen(PORT, () => console.log(`✅ API Server running at http://localhost:${PORT}`));
+app.get('/username1',(req,res)=>{
+if (req.session.username) {
+  res.send("hy i am hussain")
+}else{
+  res.send('no user name')
+}
+
+})
+app.get('/destroay',(req,res)=>{
+  req.session.destroy((error)=>{
+    if(error){
+      res.send('faild')
+    }else{
+      res.send('good')
+    }
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
