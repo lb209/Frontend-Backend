3a996e7f666bd547d8f39d2443a4b3f459d643a4
M Hussain Shahzad <hussainshahzad7744@gmail.com>
2025-12-27 10:59:10 +0500

    Send Email In Backend

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
 
 const app = express();
 const PORT = 5000;
 
-// root folder ko static bana diya
-app.use(express.static(path.join(__dirname, 'public')));
+// middleware
+app.use(express.json());
+app.use(cors());
 
-// test route
-app.get('/pu', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// Email Route
+app.post("/send-email", async (req, res) => {
+  const { email, subject, message } = req.body;
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "hussainshahzad.g254254@gmail.com", // ✅ correct
+        pass: "hvhbcfiidinvghwg",                 // app password
+      },
+    });
+
+    const mailOptions = {
+      from: "hussainshahzad.g254254@gmail.com",   // ✅ correct
+      to: email,
+      subject: subject,
+      text: message,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res.json({ message: "Email sent successfully ✅" });
+  } catch (error) {
+    console.log("EMAIL ERROR 👉", error);
+    res.status(500).json({ message: "Email not sent ❌" });
+  }
 });
 
+// start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
